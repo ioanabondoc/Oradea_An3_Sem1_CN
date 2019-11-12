b246f1a3396fc83b2d5bb0d2f15a250aa6dacb88
Viorel Ciortea <denuve11@gmail.com>
2019-11-12 13:41:24 +0200

    Lab 11.11.2019 si 12.11.2019

---
+++ MetodaJacobi/MetodaJacobi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MetodaJacobi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int[,] A = new int[3, 3] { { 10, 2, -1 }, { -2, -5, 1 }, { -3, 1, -5 } };
+            int[,] B = new int[3, 1] { { 9 }, { -1 }, { 8 } };
+            decimal[,] Alfa = new decimal[3, 3];
+            for(int i=0;i<n;i++)
+            {
+                for (int j = 0; j < n; j++)
+                    if (i != j)
+                        Alfa[i, j] = -A[i, j] / A[i, i];
+                    else
+                        Alfa[i, j] = 0;
+            }
+
+            AfisareMatrice(Alfa);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        static void AfisareMatrice(decimal[,] M)
+        {
+
+        }
+
+        static void AfisareMatrice(int[,] M)
+        {
+
+        }
+       
+    }
+}
